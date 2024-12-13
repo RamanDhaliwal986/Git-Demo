21f06afe2bb574ba207289ff789fd0d25a1ede00
Raman Dhaliwal <dramandeepkaur202@gmail.com>
2024-12-13 11:37:47 -0600

    initial Commit- App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Class Activity-13-12-24</Text>
       <StatusBar style="auto" />
     </View>
   );
